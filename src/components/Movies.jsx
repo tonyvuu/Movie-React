378a9d0526ca6f874b095b543b61bb0d828c03e2
Tony Vu <tony.vuu18@gmail.com>
2023-09-24 14:09:06 -0400

    made movie.css and transferred

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css';
+import './Movies.css';
 
 const MovieSuggestions = () => {
   const [filteredText, setFilteredText] = useState('');
